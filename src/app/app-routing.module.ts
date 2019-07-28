2e762c093c6a6680d401896851c4a92f8803ef56
momoView <momochanmo.zz@gmail.com>
2019-07-28 20:27:37 +0100

    feat(recipes-module): added recipes module
    & shared module for dropdown directive, added link for recipes in header  & changed from bootstrap version 4 to version 3 & introduce"d lazy loading to app
    
    feat in branch feat/recipes-module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 
 import { HomeComponent } from './core/home/home.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
@@ -7,11 +7,13 @@ import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
+  { path:'recipes', loadChildren:'./recipes/recipes.module#RecipesModule' },
   { path: 'shopping-list', component: ShoppingListComponent }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes,
+    { preloadingStrategy: PreloadAllModules })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
