e566ddd1fde49082ab99cc8639ad7dd8756a2ce0
momoView <momochanmo.zz@gmail.com>
2019-08-03 08:46:55 +0100

    feat(auth-module): added auth verification in
    header & auth state & reducer in app.reducers & added AuthModule & AuthEffects in app.module
    
    feat in branch #feat/auth-module-firebase

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
 
+import * as fromRecipes from '../../recipes/store/recipes.reducers';
+import * as fromAuth from '../../auth/store/auth.reducers';
+import * as authActions from '../../auth/store/auth.actions';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +12,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  authState$: Observable<fromAuth.State>;
 
-  constructor() { }
+  constructor(private store: Store<fromRecipes.FeatureState>) { }
 
   ngOnInit() {
+    this.authState$ = this.store.select('auth');
+  }
+
+  onLogout() {
+    this.store.dispatch(new authActions.Logout());
   }
 }
--- src/app/store/app.reducers.ts
+++ src/app/store/app.reducers.ts
@@ -1,11 +1,14 @@
 import { ActionReducerMap } from '@ngrx/store';
 
 import * as fromSL from '../shopping-list/store/shopping-list.reducers';
+import * as fromAuth from '../auth/store/auth.reducers';
 
 export interface AppState {
-  shoppingList: fromSL.State
+  shoppingList: fromSL.State,
+  auth: fromAuth.State
 }
 
 export const reducers: ActionReducerMap<AppState> = {
-  shoppingList: fromSL.shoppingListReducer
+  shoppingList: fromSL.shoppingListReducer,
+  auth: fromAuth.authReducer
 };
