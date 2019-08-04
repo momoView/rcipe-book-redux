9b0fc277d7c980356892637ac85d348e89d43540
momoView <momochanmo.zz@gmail.com>
2019-08-04 09:20:25 +0100

    wip feat(interceptors): added auth interceptor
    to add token to http requests & added logging interceptor to log detail about http request events
    
    feat for branch #feat/save-fetch-data-firebase

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
+import { HTTP_INTERCEPTORS } from '@angular/common/http'
 
 import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './header/header.component';
 import { AppRoutingModule } from '../app-routing.module';
 import { SharedModule } from '../shared/shared.module';
+import { AuthInterceptor } from '../shared/auth.interceptor'
+import { LoggingInterceptor } from '../shared/logging.interceptor'
 
 @NgModule({
   declarations: [
@@ -13,6 +16,12 @@ import { SharedModule } from '../shared/shared.module';
   imports: [
     SharedModule,
     AppRoutingModule
+  ],
+  providers: [
+    { provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS,
+      useClass: LoggingInterceptor, multi: true }
   ],
   exports: [
     HeaderComponent,
